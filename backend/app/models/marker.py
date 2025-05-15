ffa7299b6d2375d59a1b2f19d1d3fc212b712f57
강승진 <meffy@m1mba.local>
2025-05-15 17:31:12 +0900

    Update Marker model to include optional fields for amenties , capcity,,,

--- backend/app/models/marker.py
+++ backend/app/models/marker.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
 
 class Marker(BaseModel):
-    lat: float
-    lng: float
-    description: str
+    id: Optional[str]  # Firestore에서 자동 생성될 수 있음
+    latitude: float
+    longitude: float
+    name: str
+    description: Optional[str]
+    address: Optional[str]
+    region: Optional[str]
+    type: Optional[str]
+    status: Optional[str] = "운영 중"
+    last_updated: Optional[datetime]
+    created_at: Optional[datetime]
+    amenities: Optional[List[str]]
+    capacity: Optional[int]
+    image_url: Optional[str]
+    rating: Optional[float]
+    reviews: Optional[List[str]]
