eeb31ace03b1c210978ae6af9bc785fdaf53526e
underthesun7213 <103552820+underthesun7213@users.noreply.github.com>
2025-08-14 21:31:21 +0900

    spring-boot
    스프링 부트 푸시

---
+++ backend/neogul-map/neogul-map/src/main/java/com/neogulmap/neogul_map/NeogulMapApplication.java
@@ -0,0 +1,13 @@
+package com.neogulmap.neogul_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeogulMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeogulMapApplication.class, args);
+	}
+
+}
---
+++ backend/neogul-map/neogul-map/src/test/java/com/neogulmap/neogul_map/NeogulMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neogulmap.neogul_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeogulMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
